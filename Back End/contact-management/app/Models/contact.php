37880c3c4a72b90bd4322d3cdc03e7c5ec21e7b2
harshani <ruwanthikapemarathna@gmail.com>
2024-10-15 21:23:51 +0530

    updated database information

--- Back End/contact-management/app/Models/contact.php
+++ Back End/contact-management/app/Models/contact.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class contact extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'name',
         'address',
         'contact',
         'note'
     ];
+    
+    use HasFactory;
+
+    
 }
