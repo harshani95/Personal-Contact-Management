700226300b864bbe76707bf0b5a69726a501a5bf
harshani <ruwanthikapemarathna@gmail.com>
2024-10-22 14:06:30 +0530

    updated contact table

--- Back End/contact-management/database/migrations/2024_10_15_153808_create_contacts_table.php
+++ Back End/contact-management/database/migrations/2024_10_15_153808_create_contacts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('address');
             $table->string('contact');
-            $table->string('note');
+            $table->string('note')->nullable();
             $table->timestamps();
         });
     }
